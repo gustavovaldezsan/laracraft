3462d719f28cce09e52ed7c7f8c13b1bd8c52e06
Gustavo Valdez <gvaldezsan@gmail.com>
2014-03-28 12:17:47 -0600

    Added some Player code

--- src/Valdez/Laracraft/StarcraftPlayer.php
+++ src/Valdez/Laracraft/StarcraftPlayer.php
@@ -6,7 +6,11 @@ class StarcraftPlayer extends StarcraftObject {
 	protected $realm;
 	protected $name;
 
-	function __construct($id, $name, $realm)
+	protected $profileData;
+	protected $laddersData;
+	protected $matchesData;
+
+	function __construct($id, $realm, $name)
 	{
 		$this->id = $id;
 		$this->name = $name;
@@ -15,6 +19,28 @@ class StarcraftPlayer extends StarcraftObject {
 
 	public function populate()
 	{
+		$profileQuery = "/api/sc2/profile/$this->id/$this->realm/$this->name/";
+		$laddersQuery = $profileQuery . "ladders";
+		$matchesQuery = $profileQuery . "matches";
+
+		$this->profileData = $this->runApiCall($profileQuery);
+		$this->laddersData = $this->runApiCall($laddersQuery);
+		$this->matchesData = $this->runApiCall($matchesQuery);
+	}
+
+	public function getProfile()
+	{
+		// We just want top level data. I'll try to figure out a cleaner way of doing this.
+		$data = array(
+			'id'           => $this->profileData['id'],
+			'realm'        => $this->profileData['realm'],
+			'displayName'  => $this->profileData['displayName'],
+			'clanName'     => $this->profileData['clanName'],
+			'clanTag'      => $this->profileData['clanTag'],
+			'profilePath'  => $this->profileData['profilePath'],
+		);
+
+		return $data;
 	}
 
 }
